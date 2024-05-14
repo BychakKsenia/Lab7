1723e1616b7c652f3fb09e90b277670d15d87497
KseniaBychak <bychak.ksenia@gmail.com>
2024-05-14 16:51:46 +0300

    delete error tests

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -15,7 +15,6 @@ namespace Prime.UnitTests.Services
         [InlineData(0, 0, 1, 2)]
         [InlineData(-2, 3, 0, 0)]
         [InlineData(0, 0, 0, 0)]
-        [InlineData(1, 5, 2, 6)]
         public void IsVectorDot(double x1, double y1, double x2, double y2)
         {
             var result = _primeService.GetVectorsTypes(x1, y1, x2, y2);
@@ -26,7 +25,6 @@ namespace Prime.UnitTests.Services
         [InlineData(1, 2, 1, 2)]
         [InlineData(-1, -2, 1, 2)]
         [InlineData(1, 2, 2, 1)]
-        [InlineData(1, 5, 2, 6)]
         public void IsEqualLength(double x1, double y1, double x2, double y2)
         {
             var result = _primeService.GetVectorsTypes(x1, y1, x2, y2);
@@ -36,8 +34,6 @@ namespace Prime.UnitTests.Services
         [Theory]
         [InlineData(-1, -2, 1, 2)]
         [InlineData(-1, 4, 2, -8)]
-        [InlineData(2, 3, 4, 6)]
-        [InlineData(1, 5, 2, 6)]
         public void IsOpposite(double x1, double y1, double x2, double y2)
         {
             var result = _primeService.GetVectorsTypes(x1, y1, x2, y2);
@@ -45,7 +41,6 @@ namespace Prime.UnitTests.Services
         }
 
         [Theory]
-        [InlineData(-1, -2, 1, 2)]
         [InlineData(0, 4, 2, -8)]
         [InlineData(6, 3, 4, 6)]
         [InlineData(1, 5, 2, 6)]
